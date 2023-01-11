ff65e004bbdafbb82ce0c2b89647e6ec51aae618
Julien-QTX <julien.quatravaux@live.fr>
2023-01-11 14:05:33 +0100

    A

--- src/templates/pages/operations/deposit.php
+++ src/templates/pages/operations/deposit.php
@@ -1,8 +1,10 @@
 <?php
 // If user's not logged then go to login page
 session_start();
-if (!isset($_SESSION['logged']))
-  header('Location: login.php');
+if (!isset($_SESSION['logged'])){
+    header('Location: login.php');
+}
+  
 
 $userId = $_SESSION['logged'];
 
@@ -10,16 +12,17 @@ if(isset($_GET['account']) && !empty($_GET['account'])) {
   $accountId = htmlspecialchars($_GET['account']);
   $_SESSION['account']=$accountId;
 }
-elseif(isset($_SESSION['account']) && !empty($_SESSION['account']))
-  $accountId = htmlspecialchars($_SESSION['account']);
-
+elseif(isset($_SESSION['account']) && !empty($_SESSION['account'])){
+    $accountId = htmlspecialchars($_SESSION['account']);
+}
+  
 $type=$amount="";
-if(isset($_POST['amount']) && !empty($_POST['amount'])) 
-  $amount = htmlspecialchars($_POST['amount']);
-
-if(isset($_POST['type']) && !empty($_POST['type'])) 
-  $type = htmlspecialchars($_POST['type']);
-
+if(isset($_POST['amount']) && !empty($_POST['amount'])) {
+    $amount = htmlspecialchars($_POST['amount']);
+}
+if(isset($_POST['type']) && !empty($_POST['type'])) {
+    $type = htmlspecialchars($_POST['type']);
+}
 
 require_once "view/template/database.php";
 $db = dbConnect();
@@ -28,7 +31,9 @@ if (userVerif($db, $accountId, $userId)) {
   $Transfer = execCounterTransfer($db, $accountId, $type, $amount);
   $account = getAccount($db, $accountId);
 
-} else header('Location: login.php');
+} else {
+    header('Location: login.php');
+}
 
 
 $uName =($_SESSION['uName']);
