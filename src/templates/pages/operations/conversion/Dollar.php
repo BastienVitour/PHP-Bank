afd118fbda290bdcf0464324260c80f85e418d97
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-01-12 14:21:24 +0100

    fin conversion

--- src/templates/pages/operations/conversion/Dollar.php
+++ src/templates/pages/operations/conversion/Dollar.php
@@ -1,6 +1,6 @@
 <?php
 
-$page_title = "Conversion de monnaies";
+$page_title = "Conversion du Dollar";
 
 ob_start();
 
@@ -13,28 +13,31 @@ ob_start();
 </nav>
 <div></div>
 <div>
-	<h3 >Euro convertiseur</h3>
+	<h3 >convertiseur du Dollar</h3>
 	<hr style="border-top:1px dotted #000;"/>
 	<form method="POST" action="">
-		<div >
-			<label>Euro:</label>
+		<div>
+			<label >Dollar:</label>
 			<input type="text" name="txt_digit"/>
 			<label>Choix de la monnaie : </label>
-			<select name="currency" class="form-control">
+			<select name="currency">
 				<option value="">Selection de la monnaie</option>
-				<option value="Dollar americain">Dollar americain</option>
+				<option value="Euro">Euro</option>
 				<option value="Livre Sterling">Livre Sterling</option>
 				<option value="Bitcoin">Bitcoin</option>
 				<option value="Dollar australien">Dollar australien</option>
 				<option value="Yen japonais">Yen japonais</option>
 			</select>
 			<br /><br />
-			<center><button type="submit" name="btn_submit" class="btn btn-primary form-control" style="width:30%;">Conversion</button></center>
+			<center><button type="submit" name="btn_submit" style="width:30%;">Conversion</button></center>
 			<br />
 			
 		</div>
+
 	</form>
 </div>
+
+
 
 <?php
 if(ISSET($_POST['btn_submit'])){
@@ -42,34 +45,38 @@ if(ISSET($_POST['btn_submit'])){
 	$currency = $_POST['currency'];
 	if($digit != ""){
 		switch($currency){
-			case "Dollar americain":
-				$output = $digit * 1.074;
-				echo"<center><label class='text-success' style='font-size:25px;'>$".$output."</label></center>";
+			case "Euro":
+				$output = $digit * 0.93;
+				echo"<center><label class='text-success' style='font-size:25px;'€".$output."</label></center>";
 			break;
 			
 			case "Livre Sterling":
-				$output = $digit * 0.89;
+				$output = $digit * 0.82;
 				echo"<center><label class='text-success' style='font-size:25px;'>£".$output."</label></center>";
 			break;
 			
 			case "Bitcoin":
-				$output = $digit * 0.000059;
-				echo"<center><label class='text-success' style='font-size:25px;'>&#8383".$output."</label></center>";
+				$output = $digit * 0.000055;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#8383;".$output."</label></center>";
 			break;
 			
 			case "Dollar australien":
-				$output = $digit * 1.56;
-				echo"<center><label class='text-success' style='font-size:25px;'>&#165".$output."</label></center>";
+				$output = $digit * 1.45;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#165;".$output."</label></center>";
 			break;
 			
 			case "Yen japonais":
-				$output = $digit * 142.6;
-				echo"<center><label class='text-success' style='font-size:25px;'>&#165".$output."</label></center>";
+				$output = $digit * 130.54;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#165;".$output."</label></center>";
 			break;
 		}
 	}
 }
 
+
+
+
 $page_content = ob_get_clean();
 
 ?>
+
---
+++ src/templates/pages/operations/conversion/Euro.php
@@ -0,0 +1,82 @@
+<?php
+
+$page_title = "Conversion de l'euro";
+
+ob_start();
+
+?>
+
+<nav >
+	<div>
+		
+	</div>
+</nav>
+<div></div>
+<div>
+	<h3 >convertiseur de l'euro</h3>
+	<hr style="border-top:1px dotted #000;"/>
+	<form method="POST" action="">
+		<div>
+		<label >Euro:</label>
+			<input type="text" name="txt_digit"/>
+			<label>Choix de la monnaie : </label>
+			<select name="currency">
+				<option value="">Selection de la monnaie</option>
+				<option value="Dollar americain">Dollar americain</option>
+				<option value="Livre Sterling">Livre Sterling</option>
+				<option value="Bitcoin">Bitcoin</option>
+				<option value="Dollar australien">Dollar australien</option>
+				<option value="Yen japonais">Yen japonais</option>
+			</select>
+			<br /><br />
+			<center><button type="submit" name="btn_submit" style="width:30%;">Conversion</button></center>
+			<br />
+			
+		</div>
+
+	</form>
+</div>
+
+
+
+<?php
+if(ISSET($_POST['btn_submit'])){
+	$digit = $_POST['txt_digit'];
+	$currency = $_POST['currency'];
+	if($digit != ""){
+		switch($currency){
+			case "Dollar americain":
+				$output = $digit * 1.08;
+				echo"<center><label class='text-success' style='font-size:25px;'>$".$output."</label></center>";
+			break;
+			
+			case "Livre Sterling":
+				$output = $digit * 0.89;
+				echo"<center><label class='text-success' style='font-size:25px;'>£".$output."</label></center>";
+			break;
+			
+			case "Bitcoin":
+				$output = $digit * 0.000059;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#8383;".$output."</label></center>";
+			break;
+			
+			case "Dollar australien":
+				$output = $digit * 1.56;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#165;".$output."</label></center>";
+			break;
+			
+			case "Yen japonais":
+				$output = $digit * 140.61;
+				echo"<center><label class='text-success' style='font-size:25px;'>&#165;".$output."</label></center>";
+			break;
+		}
+	}
+}
+
+
+
+
+$page_content = ob_get_clean();
+
+?>
+
