3a85178943e5577fa353e57269ef2d0f3d881d34
Julien-QTX <julien.quatravaux@live.fr>
2023-01-13 11:01:37 +0100

    Profile

--- www/actions/profile.php
+++ www/actions/profile.php
@@ -10,3 +10,9 @@ $actual_money = $stmh->fetch();
 $stmh = $db->prepare('SELECT name FROM currencies WHERE id_currencies = ?');
 $stmh->execute([$utilisateur['id']]);
 $actual_currencie = $stmh->fetch();
+
+foreach ($actual_money as $actual_currencie) {
+    $money_currencie = array_search($usr['role'], $config);
+    echo 'Utilisateur : ' . $usr['email'] . ';    Role : ' . $config['roles'][$usr['role']] . '<br>';
+
+}
